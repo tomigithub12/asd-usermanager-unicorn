b9b7d856fca199598a34fe6ca54d082daf6ac466
tomigithub12 <gaurlalhothi@hotmail.com>
2023-01-22 21:39:56 +0100

    Test_register Methode mit laufenden erfdolgreichen Mock

--- src/test/java/com/example/manager/controller/UserControllerTest.java
+++ src/test/java/com/example/manager/controller/UserControllerTest.java
@@ -2,14 +2,21 @@ package com.example.manager.controller;
 
 import com.example.manager.domain.User;
 import com.example.manager.repository.UserRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Profile;
+import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.context.annotation.*;
 import org.springframework.util.Assert;
 
 import static org.hamcrest.CoreMatchers.instanceOf;
@@ -29,11 +36,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 
 
+
 class UserControllerTest {
 
-    //@Mock
-    //@Autowired
-    //private UserRepository repo;
+    @InjectMocks
+    UserController usercontrolla;
+
+    @Mock
+    private UserRepository repo;
+
+   @BeforeEach
+   void setUp() throws Exception {
+       MockitoAnnotations.initMocks(this);
+   }
 
     @Test
     public void exampleTest() {
@@ -45,16 +60,21 @@ class UserControllerTest {
 
 
     @Test
-    void register() {
+    void test_register() {
         User dummyUser = new User("benis","kebab","box", "asd123", 0, false, null);
 
-        //Mockito.when(repo.save(Mockito.any(User.class))).thenAnswer(dummyUser);
+        Mockito.when(repo.save(Mockito.any(User.class))).thenReturn(dummyUser);
 
+        User test_user = usercontrolla.register(dummyUser.getFirstname(), dummyUser.getLastname(), dummyUser.getUsername(), dummyUser.getPassword());
 
-        UserController test_user_controller = new UserController();
+        assertNotNull(test_user);
 
-        User test_user = test_user_controller.register(dummyUser.getFirstname(), dummyUser.getLastname(), dummyUser.getUsername(), dummyUser.getPassword());
+        assertEquals(dummyUser.getLastname(), test_user.getLastname());
 
-        assertEquals(dummyUser, test_user);
+        //UserController test_user_controller = new UserController();
+
+        //User test_user = test_user_controller.register(dummyUser.getFirstname(), dummyUser.getLastname(), dummyUser.getUsername(), dummyUser.getPassword());
+
+        //assertEquals(dummyUser, test_user);
     }
 }
