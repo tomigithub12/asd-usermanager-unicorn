e8f1e50c764f0b4adfedff4a94eadae0e305aa37
mihip <mihip@gmx.at>
2023-01-24 20:38:57 +0100

    change in UserControllerTest.java auskommentieren von import org.junit.runner.RunWith damit es compilen kann wird nicht verwendet in UserControllerTest.java

--- src/test/java/com/example/manager/controller/UserControllerTest.java
+++ src/test/java/com/example/manager/controller/UserControllerTest.java
@@ -5,7 +5,7 @@ import com.example.manager.repository.UserRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+//import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
